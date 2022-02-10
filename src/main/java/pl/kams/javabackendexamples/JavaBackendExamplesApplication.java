666bd9708837c6ba11a8ddfce79d9bb905c33143
KamilKaczmarkiewicz <kaczmarkiewicz98@gmail.com>
2022-02-10 19:24:20 +0100

    created project and added database to config

---
+++ src/main/java/pl/kams/javabackendexamples/JavaBackendExamplesApplication.java
@@ -0,0 +1,13 @@
+package pl.kams.javabackendexamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBackendExamplesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaBackendExamplesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/kams/javabackendexamples/JavaBackendExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kams.javabackendexamples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBackendExamplesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
